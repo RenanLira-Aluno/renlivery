2df398d8978fdd4882fae9c497f6dce5f5aa3749
Renan-Lira <renan.almeida@lojaspintos.com.br>
2024-01-26 13:09:06 -0300

    criacao report file

--- lib/renlivery/orders/report.ex
+++ lib/renlivery/orders/report.ex
@@ -1,4 +1,5 @@
 defmodule Renlivery.Orders.Report do
+  alias Renlivery.Item
   alias Renlivery.Order
   alias Renlivery.Repo
 
@@ -9,13 +10,28 @@ defmodule Renlivery.Orders.Report do
   def create(filename \\ "report.csv") do
     query = from(o in Order, order_by: o.user_id)
 
-    Repo.transaction(fn ->
-      query
-      |> Repo.stream(max_rows: @default_block_size)
-      |> Stream.chunk_every(@default_block_size)
-      |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
-      |> Enum.into([])
-      |> IO.inspect()
-    end)
+    {:ok, orders_string} =
+      Repo.transaction(
+        fn ->
+          query
+          |> Repo.stream(max_rows: @default_block_size)
+          |> Stream.chunk_every(@default_block_size)
+          |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
+          |> Enum.map(&parse_line/1)
+        end,
+        timeout: :infinity
+      )
+
+    File.write(filename, orders_string)
+  end
+
+  defp parse_line(%Order{user_id: user_id, payment_method: payment, items: items}) do
+    items_string = Enum.map(items, &item_string/1) |> Enum.join(";")
+
+    "#{user_id},#{payment},[#{items_string}],#{50.0}\n"
+  end
+
+  defp item_string(%Item{description: description, price: price, category: category}) do
+    "#{category},#{description},#{price}"
   end
 end
